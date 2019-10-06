a2e32296205aa9041a08a2b70dd1b684f1bc6f17
Annie-Huang <annie.hn@gmail.com>
2019-10-06 14:45:00 +1100

    create React class component

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,66 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-// const App = ({ message }) => <div>{message}</div>;
-// const App: React.FunctionComponent<{message: string}> = ({ message }) => <div>{message}</div>;
+// Stateless:
+// // const App = ({ message }) => <div>{message}</div>;
+// // const App: React.FunctionComponent<{message: string}> = ({ message }) => <div>{message}</div>;
+// type AppProps = {message: string};
+// const App: React.FunctionComponent<AppProps> = ({ message }) => <div>{message}</div>;
+
+////////////////////////////////////////
+
+// Stateful:
+// class App extends React.Component {
+//     // render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+//     render() {
+//         return <div> Hello world</div>;
+//     }
+// }
+
+// class App extends React.Component<{message: string}> {
+//     render() {
+//         return <div>{this.props.message}</div>;
+//     }
+// }
+
+// class App extends React.Component<{message: string}, {count: number}> {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             count: 0
+//         }
+//     }
+//     render() {
+//         return <div onClick={this.increment}>{this.props.message} {this.state.count}</div>;
+//     }
+//
+//     increment = () => {
+//         this.setState({
+//             count: this.state.count + 1
+//         })
+//     }
+// }
 
 type AppProps = {message: string};
-const App: React.FunctionComponent<AppProps> = ({ message }) => <div>{message}</div>;
+type AppState = {count: number}
+class App extends React.Component<AppProps, AppState> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+    render() {
+        return <div onClick={this.increment}>{this.props.message} {this.state.count}</div>;
+    }
+    increment = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+}
 
 ReactDOM.render(
-    <App message="Hello world" />,
+    <App message="Hello again" />,
     document.getElementById('root')
 );
