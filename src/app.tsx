aa2ad78a0ab4422f7a09f44974be6fc70437826e
Annie-Huang <annie.hn@gmail.com>
2019-10-06 14:30:46 +1100

    create Reaction functional component

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello world</div>, document.getElementById('root'));
+// const App = ({ message }) => <div>{message}</div>;
+// const App: React.FunctionComponent<{message: string}> = ({ message }) => <div>{message}</div>;
+
+type AppProps = {message: string};
+const App: React.FunctionComponent<AppProps> = ({ message }) => <div>{message}</div>;
+
+ReactDOM.render(
+    <App message="Hello world" />,
+    document.getElementById('root')
+);
